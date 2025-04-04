98997c887870c38ea990a873dc2bbdbc9190dece
bekhrubzek <h995648@gmail.com>
2025-04-05 00:23:35 +0200

    almostwork!!!!!!!!!!!!!!

--- listener.js
+++ listener.js
@@ -1,20 +1,13 @@
 let counter = 0 
-const button = document.querySelector(".btn");
- button.addEventListener ('click', () => {
-     console.log(" first button click");
-});
- button.addEventListener ('click', () => {
-     console.log(" second button click");
-});
-
+const button = document.querySelector(".button");
 // text content 
 let text = document.getElementById("demo").textContent;
 
-
-function chetoclicker()
+function handleclicker()
 {
     counter=counter+1
-    document.getElementById("button").textContent = counter;
+    button.textContent = counter;
+    
 
 }
 document.getElementById('button')
